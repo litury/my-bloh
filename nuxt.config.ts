70959d6661ac14d758fd8af9870c1a282d538ddd
litury <litury@icloud.com>
2023-04-03 14:12:01 +0500

    chore: add styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,10 @@
+// ./nuxt.config.ts
+import { defineNuxtConfig } from "nuxt/config";
+
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content']
+  modules: ['@nuxt/content', '@nuxtjs/tailwindcss'],
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+  }
 })
